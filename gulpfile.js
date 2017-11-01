c508b8a64d50ef19a6169d7d2963142655fe08fd
Jeff Browning <jeff@craftcoded.com>
2017-10-31 22:12:06 -0700

    initial file commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+	return gulp.src(['src/scss/*.scss'])
+		.pipe(sass())
+		.pipe(gulp.dest("src/css"))
+		.pipe(browserSync.stream());
+	});
+
+//Setup transpile of ES6 --> ES5
+gulp.task('es6', function () {
+    return gulp.src('src/js/*.js')
+        .pipe(babel({
+			presets: ['env'],
+            ignore: ['./src/js/bootstrap.min.js','./src/js/popper.min.js','./src/js/jquery.min.js']
+        }))
+        .pipe(gulp.dest("./src/dist/js"))
+        .pipe(browserSync.stream());
+});
+
+//Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'es6'], function() {
+	browserSync.init({
+		// Will not attempt to determine your network status, assumes you're OFFLINE
+//		online: false,
+		server: "./src"
+		});
+
+	gulp.watch(['src/scss/*.scss'],['sass']);
+	gulp.watch(['src/js/*.js'],['es6']);
+	gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default',['serve']);
+
